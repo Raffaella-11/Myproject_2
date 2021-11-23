b4eb605a9c84c6b60f22016e50773f8988f65f17
LamanBrik <erik.brovelli@mail.polimi.it>
2021-11-23 19:19:00 +0100

    Path treno sistemato

--- Assets/Scripts/train_move.cs
+++ Assets/Scripts/train_move.cs
@@ -5,11 +5,26 @@ using PathCreation;
 
 public class train_move : MonoBehaviour
 {
-    public PathCreator pathCreator;
-    public float speed = 5;
-    public float distanceTravelled;
-
+    public PathCreator pathCreator; //path che il treno seguirà
+    
+    //dichiaro variabili a cui associo i 2 path diversi
+    public GameObject pathLeftObj; 
+    public GameObject pathRightObj;
+    
+    public float speed = 3;
+    private float distanceTravelled;
     public EndOfPathInstruction end;
+    
+    private Vector3 startPosition = new Vector3(0.4f, 0, 0); //posizione iniziale trenino
+    private int startTrain = 0; //serve come variabile flag per evitare che il treno parta prima di aver scelto uno dei due path
+    private Quaternion offset; //per sistemare la rotazione del trenino lungo il path
+
+    //confronta variabile vector3 evitando errori di approssimazione
+    public bool V3Equal(Vector3 a, Vector3 b)
+    {
+        return Vector3.SqrMagnitude(a - b) < 0.0001;
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +34,34 @@ public class train_move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distanceTravelled += speed * Time.deltaTime;
-        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, end);
-        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, end);
+        if (Input.GetKey(KeyCode.A) & startTrain == 0) //impedisce input mentre il treno si muove
+        {
+            pathCreator = pathLeftObj.GetComponent<PathCreator>(); //cambia path
+            offset = pathCreator.path.GetRotation(1); //cambia offset
+            
+            distanceTravelled = 0; //azzera la distanza
+            startTrain = 1; //entra nell'if di movimento del treno
+        }
 
+        if (Input.GetKey(KeyCode.D) & startTrain == 0)
+        {
+            pathCreator = pathRightObj.GetComponent<PathCreator>();
+            offset = pathCreator.path.GetRotation(1);
+            
+            distanceTravelled = 0;
+            startTrain = 1;
+        }
+
+        if (startTrain == 1)
+        {
+            distanceTravelled += speed * Time.deltaTime;
+            transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled, end);
+            transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled, end) * offset * offset;
+
+            if (V3Equal(transform.position,startPosition))
+            {
+                startTrain = 0; //riattiva possibilità input del player
+            }
+        }
     }
 }
