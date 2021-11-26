f637d87d53c91c34106eb78728510cad17964d90
ClaudiaCordara <claudia.cordara@mail.polimi.it>
2021-11-26 09:50:57 +0100

    camera following train

--- Assets/Scripts/cam_move.cs
+++ Assets/Scripts/cam_move.cs
@@ -6,9 +6,55 @@ using PathCreation;
 
 public class cam_move : MonoBehaviour
 {
-    public GameObject player;
+    [SerializeField]
+    private Transform target;
+ 
+    [SerializeField]
+    private Vector3 offsetPosition;
+ 
+    [SerializeField]
+    private Space offsetPositionSpace = Space.Self;
+ 
+    [SerializeField]
+    private bool lookAt = true;
+ 
+    private void Update()
+    {
+        Refresh();
+    }
+ 
+    public void Refresh()
+    {
+        if(target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+ 
+            return;
+        }
+ 
+        // compute position
+        if(offsetPositionSpace == Space.Self)
+        {
+            transform.position = target.TransformPoint(offsetPosition);
+        }
+        else
+        {
+            transform.position = target.position + offsetPosition;
+        }
+ 
+        // compute rotation
+        if(lookAt)
+        {
+            transform.LookAt(target);
+        }
+        else
+        {
+            transform.rotation = target.rotation;
+        }
+    }
+    //public GameObject player;
 
-    private Vector3 offsetPosition = new Vector3(-1.4f, 0.8f, -0.0f); //posizione iniziale camera
+    //private Vector3 offsetPosition = new Vector3(-1.4f, 0.6f, -0.0f); //posizione iniziale camera
     //private Quaternion offsetRotation;// = new Vector3(15, 90, 0);
 
     // Start is called before the first frame update
@@ -18,9 +64,9 @@ public class cam_move : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        transform.position = player.transform.position + offsetPosition;
-        //transform.rotation = player.transform.rotation;// * offsetRotation;// * Quaternion.Euler(offsetRotation);
-    }
+    //void Update()
+    //{
+        //transform.position = player.transform.position + offsetPosition;
+        //transform.rotation = player.transform.rotation; //* offsetRotation;// * Quaternion.Euler(offsetRotation);
+   // }
 }
